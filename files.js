68d52cf366756193d9547d278736348183d022c9
Victoria <vickimarji@icloud.com>
2022-08-15 22:11:36 -0400

    new push

---
+++ files.js
@@ -0,0 +1,18 @@
+// const fs = require('fs');
+
+// fs.readFile('./docs/blog1.txt', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+
+// console.log('last line');
+
+
+fs.writeFile('./docs/blog1.txt', 'hello world', () => {
+    console.log('file was written');
+});
+fs.writeFile('./docs/blog2.txt', 'hello world', () => {
+    console.log('file was written');
+});
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['harry', 'mike', 'kevin', 'mark'];
+const ages = [22, 87, 43, 19];
+console.log(people);
+
+module.exports = {
+    people, ages
+}
